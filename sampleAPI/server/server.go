bb51dc83ac491af8455cd4a84cc82b6752457615
take-2405 <take12.wa@icloud.com>
2019-12-30 00:57:33 +0900

    [fix]sampleAPI

--- sampleAPI/server/server.go
+++ sampleAPI/server/server.go
@@ -1,28 +1,28 @@
 package server
 
 import (
-    "github.com/gin-gonic/gin"
-    "github.com/git/Go-Practice/sampleAPI/controller"
+	"github.com/gin-gonic/gin"
+	user "github.com/git/Go-Practice/sampleAPI/controller"
 )
 
 // Init is initialize server
-func Init() {
-    r := router()
-    r.Run()
+func init() {
+	r := router()
+	r.Run()
 }
 
 func router() *gin.Engine {
-    r := gin.Default()
+	r := gin.Default()
 
-    u := r.Group("/users")
-    {
-        ctrl := user.Controller{}
-        u.GET("", ctrl.Index)
-        u.GET("/:id", ctrl.Show)
-        u.POST("", ctrl.Create)
-        u.PUT("/:id", ctrl.Update)
-        u.DELETE("/:id", ctrl.Delete)
-    }
+	u := r.Group("/users")
+	{
+		ctrl := user.Controller{}
+		u.GET("", ctrl.Index)
+		u.GET("/:id", ctrl.Show)
+		u.POST("", ctrl.Create)
+		u.PUT("/:id", ctrl.Update)
+		u.DELETE("/:id", ctrl.Delete)
+	}
 
-    return r
+	return r
 }
